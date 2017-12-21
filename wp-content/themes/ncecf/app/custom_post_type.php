964710b76696d6a0b5c7bb0a5d5fabc00099f826
Daniel Griffiths <logiconictvtube@gmail.com>
2017-12-21 11:01:15 -0500

    initial commit

---
+++ wp-content/themes/ncecf/app/custom_post_type.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App;
+
+function create_post_type() {
+
+  $argsIssues = array(
+    'labels' => array(
+      'name' => __( 'Issues' ),
+      'singular_name' => __( 'Issue' )
+    ),
+    'public' => true,
+    'exclude_from_search' => false,
+    'publicly_queryable' => true,
+    'show_ui' => true,
+    'show_in_nav_menus' => true,
+    'menu_position' => 20,
+    'menu_icon' => 'dashicons-paperclip',
+    'capability_type' => 'page',
+    'hierarchical' => false,
+    'supports' => array(
+      'title',
+      'editor',
+      'author',
+      'revisions',
+      'page-attributes'
+    ),
+    'has_archive' => false,
+    'rewrite' => true
+  );
+  register_post_type( 'issue', $argsIssues );
+
+
+  $argsInitiatives = array(
+    'labels' => array(
+      'name' => __( 'Initiatives' ),
+      'singular_name' => __( 'Initiative' )
+    ),
+    'public' => true,
+    'exclude_from_search' => false,
+    'publicly_queryable' => true,
+    'show_ui' => true,
+    'show_in_nav_menus' => true,
+    'menu_position' => 21,
+    'menu_icon' => 'dashicons-lightbulb',
+    'capability_type' => 'page',
+    'hierarchical' => false,
+    'supports' => array(
+      'title',
+      'editor',
+      'author',
+      'revisions',
+      'page-attributes'
+    ),
+    'has_archive' => false,
+    'rewrite' => true
+  );
+  register_post_type( 'initiative',  $argsInitiatives );
+
+  $argsEvents = array(
+    'labels' => array(
+      'name' => __( 'Events' ),
+      'singular_name' => __( 'Event' )
+    ),
+    'public' => true,
+    'exclude_from_search' => false,
+    'publicly_queryable' => true,
+    'show_ui' => true,
+    'show_in_nav_menus' => false,
+    'menu_position' => 22,
+    'menu_icon' => 'dashicons-store',
+    'capability_type' => 'page',
+    'hierarchical' => false,
+    'supports' => array(
+      'title',
+      'editor',
+      'author',
+      'revisions',
+      'page-attributes'
+    ),
+    'has_archive' => true,
+    'rewrite' => false
+  );
+  register_post_type( 'event', $argsEvents );
+
+
+  $argsResources = array(
+    'labels' => array(
+      'name' => __( 'Resources' ),
+      'singular_name' => __( 'Resource' )
+    ),
+    'public' => true,
+    'exclude_from_search' => false,
+    'publicly_queryable' => true,
+    'show_ui' => true,
+    'show_in_nav_menus' => false,
+    'menu_position' => 23,
+    'menu_icon' => 'dashicons-media-document',
+    'capability_type' => 'page',
+    'hierarchical' => false,
+    'supports' => array(
+      'title',
+      'editor',
+      'author',
+      'revisions',
+      'page-attributes'
+    ),
+    'has_archive' => true,
+    'rewrite' => true
+  );
+  register_post_type( 'resource', $argsResources );
+
+}
+
+add_action( 'init', __NAMESPACE__.'\\create_post_type' );
---
+++ wp-content/themes/ncecf/app/custom_taxonomy.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+function create_taxonomies() {
+
+	$argsResourceType = array(
+		'labels' => array(
+			'name' => __( 'Resource Types' ),
+			'singular_name' => __( 'Resource Type' )
+		),
+		'publicly_queryable' => true,
+		'show_ui' => true,
+		'show_in_nav_menus' => false,
+		'hierarchical' => false,
+		'rewrite' => true
+	);
+	register_taxonomy('resource-type', 'resource', $argsResourceType);
+
+	$argsIssueCategories = array(
+		'labels' => array(
+			'name' => __( 'Issue Categories' ),
+			'singular_name' => __( 'Issue Category' )
+		),
+		'publicly_queryable' => true,
+		'show_ui' => true,
+		'show_in_nav_menus' => false,
+		'hierarchical' => false,
+		'rewrite' => true
+	);
+	register_taxonomy('issue-category', 'issue', $argsIssueCategories);
+
+}
+
+add_action( 'init', __NAMESPACE__.'\\create_taxonomies' );
