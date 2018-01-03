a5973a1b2390a39afea91ba187d12710e77aae75
Daniel Griffiths <logiconictvtube@gmail.com>
2018-01-03 10:15:35 -0500

    footer content done, needs php functionality

--- wp-content/themes/ncecf/resources/views/partials/footer.blade.php
+++ wp-content/themes/ncecf/resources/views/partials/footer.blade.php
@@ -9,6 +9,7 @@
         First Name: <input type="text"><br>
         Last Name: <input type="text"><br>
         Email: <input type="text"><br>
+        <button type="button">Sign Up</button>
       </form>
     </div>
     @php(dynamic_sidebar('sidebar-footer'))
