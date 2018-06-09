bc71b66c1962571721a773a97fae1007771e2e7c
Alisa Herr <alisa@unitymakes.us>
2018-06-09 11:15:46 -0400

    Nofollow Unity link

--- wp-content/themes/ncecf/resources/views/partials/unity.blade.php
+++ wp-content/themes/ncecf/resources/views/partials/unity.blade.php
@@ -1,3 +1,3 @@
-<a href="https://www.unitymakes.us/" target="_blank" class="unity-link" rel="noopener">
+<a href="https://www.unitymakes.us/" target="_blank" class="unity-link" rel="noopener nofollow">
   {{ \BladeSvgSage\svg_image('powered-by-unity') }}
 </a>
